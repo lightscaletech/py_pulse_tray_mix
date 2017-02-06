4526fd89c641425f529fcb2b9329427d3a0877b2
Sam Light <samlight1994@gmail.com>
2017-02-06 22:17:06 +0000

    Get possition of system tray icon

--- py_pulse_tray_mixer/application.py
+++ py_pulse_tray_mixer/application.py
@@ -1,13 +1,14 @@
 import os
 from PyQt5 import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (QApplication, QWidget, QSystemTrayIcon)
+from PyQt5.QtWidgets import (QApplication, QWidget, QSystemTrayIcon,
+                             QDesktopWidget)
 
 class Application(QApplication):
     def __init__(self, args):
         QApplication.__init__(self, args)
         self.trayIcon = TrayIcon()
-        self.mixWin = MixerWindow()
+        self.mixWin = MixerWindow(self.trayIcon)
 
         self.trayIcon.activated.connect(self.mixWin.toggle)
 
@@ -16,17 +17,45 @@ class Application(QApplication):
         del self.mixWin
 
 class MixerWindow(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, trayicon, parent=None):
         QWidget.__init__(self, parent, Qt.Qt.Dialog | Qt.Qt.FramelessWindowHint)
+        self.trayicon = trayicon
         self.setWindowTitle("Mixer")
-        self.show()
+
+    def reposition(self):
+        pos = self.trayicon.getPos()
+        print(pos)
+        pass
 
     def toggle(self):
         if self.isVisible(): self.hide()
-        else: self.show()
+        else:
+            self.reposition()
+            self.show()
 
 class TrayIcon(QSystemTrayIcon):
+
+    class Possition:
+        TOP    = 1
+        LEFT   = 2
+        BOTTOM = 4
+        RIGHT  = 8
+
     def __init__(self, parent=None):
         icon = QIcon(os.path.dirname(__file__) + '/../img/trayicon.png')
         QSystemTrayIcon.__init__(self, icon, parent)
         self.show()
+
+    def getPos(self):
+        screenRect = QApplication.desktop().screenGeometry()
+        iconRect = self.geometry()
+        hheight = screenRect.height() / 2
+        hwidth = screenRect.width() / 2
+        iconX = iconRect.x()
+        iconY = iconRect.y()
+        res = 0
+        if iconX < hwidth: res |= self.Possition.LEFT
+        else: res |= self.Possition.RIGHT
+        if iconY < hheight: res |= self.Possition.TOP
+        else: res |= self.Possition.BOTTOM
+        return res
