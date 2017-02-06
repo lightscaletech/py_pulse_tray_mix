674b04f182a55ea576b7aafeb1b96acb28052323
Sam Light <samlight1994@gmail.com>
2017-02-06 21:44:51 +0000

    Created tray icon and basic borderless window.

--- py_pulse_tray_mixer/application.py
+++ py_pulse_tray_mixer/application.py
@@ -1,10 +1,19 @@
+import os
 from PyQt5 import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QSystemTrayIcon, QIcon)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QWidget, QSystemTrayIcon)
 
 class Application(QApplication):
     def __init__(self, args):
         QApplication.__init__(self, args)
+        self.trayIcon = TrayIcon()
         self.mixWin = MixerWindow()
+
+        self.trayIcon.activated.connect(self.mixWin.toggle)
+
+    def __del__(self):
+        del self.trayIcon
+        del self.mixWin
 
 class MixerWindow(QWidget):
     def __init__(self, parent=None):
@@ -12,4 +21,12 @@ class MixerWindow(QWidget):
         self.setWindowTitle("Mixer")
         self.show()
 
-class TrayIcon
+    def toggle(self):
+        if self.isVisible(): self.hide()
+        else: self.show()
+
+class TrayIcon(QSystemTrayIcon):
+    def __init__(self, parent=None):
+        icon = QIcon(os.path.dirname(__file__) + '/../img/trayicon.png')
+        QSystemTrayIcon.__init__(self, icon, parent)
+        self.show()
