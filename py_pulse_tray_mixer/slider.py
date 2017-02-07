ffaa65e78f0ca3f8fdee4f373c590ea565e13fab
Sam Light <samlight1994@gmail.com>
2017-02-07 23:27:46 +0000

    Created a basic channel slider

--- py_pulse_tray_mixer/slider.py
+++ py_pulse_tray_mixer/slider.py
@@ -1,12 +1,24 @@
 from PyQt5.Qt import Qt
-from PyQt5.QtWidgets import (QWidget, QLabel, QSlider,
+from PyQt5.QtWidgets import (QWidget, QLabel, QSlider, QPushButton,
                              QHBoxLayout, QVBoxLayout)
 
 class Slider(QWidget):
 
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
-        slider = QSlider(Qt.Vertical, self)
+        self.control = QSlider(Qt.Vertical, self)
+        self.icon = QLabel('Icon', self)
+        self.title = QLabel('Title', self)
+        self.muteBtn = QPushButton('M', self)
+        self.muteBtn.setMaximumWidth(20)
+
         layout = QVBoxLayout()
-        layout.addWidget(slider)
+        layout.addWidget(self.icon)
+        layout.addWidget(self.control)
+        layout.addWidget(self.title)
+
+        botLayout = QHBoxLayout()
+        botLayout.addWidget(self.muteBtn)
+
+        layout.addLayout(botLayout)
         self.setLayout(layout)
