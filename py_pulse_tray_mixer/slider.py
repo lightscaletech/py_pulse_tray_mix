57450949a91acf4a3ae75889e0aca39d04661cb0
Sam Light <samlight1994@gmail.com>
2017-02-07 07:49:01 +0000

    Made a basic slider

--- py_pulse_tray_mixer/slider.py
+++ py_pulse_tray_mixer/slider.py
@@ -1,11 +1,12 @@
 from PyQt5.Qt import Qt
-from PyQt5.QtWidgets import (QWidget, QLabel, QSlider, QButton,
+from PyQt5.QtWidgets import (QWidget, QLabel, QSlider,
                              QHBoxLayout, QVBoxLayout)
 
 class Slider(QWidget):
 
     def __init__(self, parent=None):
-        QWidget.__init__(parent)
-        slider = QWidget(Qt.Vertical, self)
+        QWidget.__init__(self, parent)
+        slider = QSlider(Qt.Vertical, self)
         layout = QVBoxLayout()
+        layout.addWidget(slider)
         self.setLayout(layout)
