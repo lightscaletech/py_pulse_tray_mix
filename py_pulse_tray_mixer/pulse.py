371bdbb005b0240a58a2652314006500ce9ad29b
Sam Light <samlight1994@gmail.com>
2017-02-10 08:34:58 +0000

    Made Pulse into class

--- py_pulse_tray_mixer/pulse.py
+++ py_pulse_tray_mixer/pulse.py
@@ -68,95 +68,98 @@ class Manager(object):
         if self.old is not None: self.old(index)
 
 
-sink_manager = Manager()
-input_manager = Manager()
+class Pulse(QOject):
+    sink_manager = Manager()
+    input_manager = Manager()
 
-_pa_ml = None
-_pa_ctx = None
+    _pa_ml = None
+    _pa_ctx = None
 
-def _info_manage(t, manager, i):
-    info = None
-    try:
-        info = i.contents
-    except: pass
-    if info is None: return
+    _c_ctx_sub_success = pa.pa_context_success_cb_t( self._ctx_sub_success )
+    _c_ctx_sub_cb      = pa.pa_context_subscribe_cb_t( self._ctx_sub_cb )
+    _c_ctx_state       = pa.pa_context_notify_cb_t( self._ctx_state )
+    _c_ctx_event       = pa.pa_context_event_cb_t( self._ctx_event )
+    _c_sink_info       = pa.pa_sink_info_cb_t( self._sink_info )
+    _c_input_info      = pa.pa_sink_input_info_cb_t( self._input_info )
 
-    manager.upsert_item(t(info))
+    @staticmethod
+    def _upsert_manage(t, manager, i):
+        info = None
+        try:
+            info = i.contents
+        except: pass
+        if info is None: return
 
-def _sink_info(ctx, sink_info, eol, ud):
-    _info_manage(Sink, sink_manager, sink_info)
+        manager.upsert_item(t(info))
 
-def _input_info(ctx, input_info, eol, ud):
-    _info_manage(Sink, input_manager, input_info)
+    def _sink_info(self, ctx, sink_info, eol, ud):
+        self._upsert_manage(Sink, sink_manager, sink_info)
 
-def _ctx_sub_success(ctx, success, ud): pass
+    def _input_info(self, ctx, input_info, eol, ud):
+        self._upsert_manage(Sink, input_manager, input_info)
 
-def _ctx_sub_cb(ctx, evt, eid, ud):
-    evtype = evt & pa.PA_SUBSCRIPTION_EVENT_TYPE_MASK
-    evfac = evt & pa.PA_SUBSCRIPTION_EVENT_FACILITY_MASK
+    def _ctx_sub_success(self, ctx, success, ud): pass
 
-    if(evfac == pa.PA_SUBSCRIPTION_EVENT_SINK):
-        if evtype == pa.PA_SUBSCRIPTION_EVENT_REMOVE:
-            sink_manager.remove_item(eid)
-        else: _request_sink_state(ctx, eid, evtype)
-    if(evfac == pa.PA_SUBSCRIPTION_EVENT_SINK_INPUT):
-        if evtype == pa.PA_SUBSCRIPTION_EVENT_REMOVE:
-            input_manager.remove_item(eid)
-        else: _request_input_state(ctx, eid, evtype)
+    def _ctx_sub_cb(self, ctx, evt, eid, ud):
+        evtype = evt & pa.PA_SUBSCRIPTION_EVENT_TYPE_MASK
+        evfac = evt & pa.PA_SUBSCRIPTION_EVENT_FACILITY_MASK
 
-def _ctx_event(ctx, name, prop_list, ud): print("Context event")
+        if(evfac == pa.PA_SUBSCRIPTION_EVENT_SINK):
+            if evtype == pa.PA_SUBSCRIPTION_EVENT_REMOVE:
+                self.sink_manager.remove_item(eid)
+            else: self._request_sink_state(ctx, eid, evtype)
+        if(evfac == pa.PA_SUBSCRIPTION_EVENT_SINK_INPUT):
+            if evtype == pa.PA_SUBSCRIPTION_EVENT_REMOVE:
+                self.input_manager.remove_item(eid)
+            else: self._request_input_state(ctx, eid, evtype)
 
-def _ctx_state(ctx, ud):
-    state = pa.pa_context_get_state(ctx)
+    def _ctx_event(self, ctx, name, prop_list, ud): print("Context event")
 
-    if state == pa.PA_CONTEXT_READY:
-        _request_sinks(ctx, pa.PA_SUBSCRIPTION_EVENT_NEW)
-        _request_inputs(ctx, pa.PA_SUBSCRIPTION_EVENT_NEW)
-        pa.pa_context_set_subscribe_callback(ctx, _c_ctx_sub_cb ,None)
-        pa.pa_context_subscribe(ctx,
-                                pa.PA_SUBSCRIPTION_MASK_SINK |
-                                pa.PA_SUBSCRIPTION_MASK_SINK_INPUT,
-                                _c_ctx_sub_success, None)
-        print ("Ready!")
+    def _ctx_state(self, ctx, ud):
+        state = pa.pa_context_get_state(ctx)
 
-_c_ctx_sub_success = pa.pa_context_success_cb_t( _ctx_sub_success )
-_c_ctx_sub_cb      = pa.pa_context_subscribe_cb_t( _ctx_sub_cb )
-_c_ctx_state       = pa.pa_context_notify_cb_t( _ctx_state )
-_c_ctx_event       = pa.pa_context_event_cb_t( _ctx_event )
-_c_sink_info       = pa.pa_sink_info_cb_t( _sink_info )
-_c_input_info      = pa.pa_sink_input_info_cb_t( _input_info )
+        if state == pa.PA_CONTEXT_READY:
+            self._request_sinks(ctx, pa.PA_SUBSCRIPTION_EVENT_NEW)
+            self._request_inputs(ctx, pa.PA_SUBSCRIPTION_EVENT_NEW)
+            pa.pa_context_set_subscribe_callback(ctx, self._c_ctx_sub_cb ,None)
+            pa.pa_context_subscribe(ctx,
+                                    pa.PA_SUBSCRIPTION_MASK_SINK |
+                                    pa.PA_SUBSCRIPTION_MASK_SINK_INPUT,
+                                    _c_ctx_sub_success, None)
+            print ("Ready!")
 
+    def _request_sinks(self, ctx, event=None):
+        pa.pa_context_get_sink_info_list(ctx, self._c_sink_info, event)
 
-def _request_sinks(ctx, event=None):
-    pa.pa_context_get_sink_info_list(ctx, _c_sink_info, event)
+    def _request_sink_state(self, ctx, id, event=None):
+        pa.pa_context_get_sink_info_by_index(ctx, id, self._c_sink_info, event)
 
-def _request_sink_state(ctx, id, event=None):
-    pa.pa_context_get_sink_info_by_index(ctx, id, _c_sink_info, event)
+    def _request_inputs(self, ctx, event=None):
+        pa.pa_context_get_sink_input_info_list(ctx, self._c_input_info, event)
 
-def _request_inputs(ctx, event=None):
-    pa.pa_context_get_sink_input_info_list(ctx, _c_input_info, event)
-
-def _request_input_state(ctx, id, event=None):
-    pa.pa_context_get_sink_input_info(ctx, id, _c_input_info, event)
-
-
-def pause(): pa.pa_threaded_mainloop_lock(_pa_ml)
-def resume(): pa.pa_threaded_mainloop_unlock(_pa_ml)
-
-def start():
-    _pa_ml = pa.pa_threaded_mainloop_new()
-    ml_api = pa.pa_threaded_mainloop_get_api(_pa_ml)
-    _pa_ctx = pa.pa_context_new(ml_api, c.c_char_p(b'Py_tray_mixer'))
-    rt = pa.pa_context_connect(_pa_ctx, None, pa.PA_CONTEXT_NOFLAGS, None)
-
-    rt = c.c_int()
-    pa.pa_threaded_mainloop_start(_pa_ml)
-
-    pa.pa_context_set_event_callback(_pa_ctx, _c_ctx_event, None)
-    pa.pa_context_set_state_callback(_pa_ctx, _c_ctx_state, None)
+    def _request_input_state(self, ctx, id, event=None):
+        pa.pa_context_get_sink_input_info(ctx, id, self._c_input_info, event)
 
 
-def stop():
-    pa.pa_context_disconnect(_pa_ctx)
-    pa.pa_threaded_mainloop_stop(_pa_ml)
-    pa.pa_threaded_mainloop_free(_pa_ml)
+    def pause(self): pa.pa_threaded_mainloop_lock(self._pa_ml)
+    def resume(self): pa.pa_threaded_mainloop_unlock(self._pa_ml)
+
+    def start(self):
+        self._pa_ml = pa.pa_threaded_mainloop_new()
+
+        ml_api = pa.pa_threaded_mainloop_get_api(self_pa_ml)
+        self._pa_ctx = pa.pa_context_new(ml_api, c.c_char_p(b'Py_tray_mixer'))
+
+        rt = pa.pa_context_connect(self._pa_ctx, None, pa.PA_CONTEXT_NOFLAGS, None)
+
+        rt = c.c_int()
+        pa.pa_threaded_mainloop_start(self._pa_ml)
+
+        pa.pa_context_set_event_callback(self._pa_ctx, self._c_ctx_event, None)
+        pa.pa_context_set_state_callback(self._pa_ctx, self._c_ctx_state, None)
+
+
+    def stop(self):
+        pa.pa_context_disconnect(self._pa_ctx)
+        pa.pa_threaded_mainloop_stop(self._pa_ml)
+        pa.pa_threaded_mainloop_free(self._pa_ml)
