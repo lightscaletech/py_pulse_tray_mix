a57f38542b7931d3b669dbfaf68f615e60f61302
Sam Light <samlight1994@gmail.com>
2017-02-11 12:57:07 +0000

    Setup mainloop and signals and slots.

--- py_pulse_tray_mixer/pulse.py
+++ py_pulse_tray_mixer/pulse.py
@@ -1,19 +1,22 @@
 from py_pulse_tray_mixer import lib_pulseaudio as pa
-from PyQt5.QtCore import QThread, QMutex, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QThread, QMutex, QObject, pyqtSignal, pyqtSlot
 import ctypes as c
 
 
-class PObj(object):
+class PObj(QObject):
     def __init__(self, info):
+        QObject.__init__(self, None)
         self.name = info.name.decode('utf-8')
+        self.title = ""
         self.index = info.index
         self.cvolume = info.volume
         self.volume = pa.pa_cvolume_avg(self.cvolume)
         self.proplist = info.proplist
 
     def get_from_proplist(self, key):
-        key = c.c_char_p(key.encode('utf-8'))
-        pa.pa_proplist_gets(self.proplist, key).decode('utf-8')
+        key = c.c_char_p(key.encode('latin-1'))
+        res = pa.pa_proplist_gets(self.proplist, key)
+        if type(res) is bytes: return res.decode('utf-8')
 
     def print_proplist(self):
         state = c.c_void_p(None)
@@ -31,6 +34,7 @@ class Sink(PObj):
     def __init__(self, info):
         PObj.__init__(self, info)
         self.icon = self.get_from_proplist(pa.PA_PROP_DEVICE_ICON_NAME)
+        self.title = self.get_from_proplist(u'alsa.card_name')
 
 class Input(PObj):
     def __init__(self, info):
@@ -38,15 +42,21 @@ class Input(PObj):
         self.name = self.get_from_proplist(pa.PA_PROP_APPLICATION_NAME)
         self.icon = self.get_from_proplist(pa.PA_PROP_APPLICATION_ICON_NAME)
 
-class Manager(object):
+class Manager(QObject):
     _items = {}
+
+    added   = pyqtSignal(PObj)
+    changed = pyqtSignal(PObj)
+    removed = pyqtSignal(int)
 
     def add_item(self, item):
         self._items[item.index] = item
         print ("Item added: " + str(item.index) + ', ' + item.name)
+        self.added.emit(item)
 
     def changed_item(self, item):
         self._items[item.index] = item
+        self.changed.emit(item)
 
     def upsert_item(self, item):
         if item.index in self._items: self.changed_item(item)
@@ -54,64 +64,84 @@ class Manager(object):
 
     def remove_item(self, index):
         print ("Item removed: %i" % index)
+        if index not in self._items: return
         del self._items[index]
+        self.removed.emit(index)
 
 class PulseMainloop(QObject):
 
     class Worker(QObject):
         def __init__(self, ml):
-            self.ml
-            self.run = QMutex()
+            QObject.__init__(self, None)
+            self.ml = ml
+            self.runLock = QMutex()
+            self.run = False
+
+        def __del__(self):
+            del self.run
+
+        def set_run(self, val):
+            self.runLock.lock()
+            self.run = val
+            self.runLock.unlock()
+
+        def get_run(self):
+            self.runLock.lock()
+            val = self.run
+            self.runLock.unlock()
+            return val
 
         def stop(self):
-            self.run.unlock()
+            print("Worker stopping")
+            self.set_run(False)
+            pa.pa_mainloop_wakeup(self.ml)
 
         @pyqtSlot()
         def start(self):
-            self.run.lock()
+            print("started")
+            self.set_run(True)
             ret = c.c_int(0)
-            while self.run.tryLock():
+            while self.get_run():
                 pa.pa_mainloop_iterate(self.ml, 1, c.pointer(ret))
+            print("stopped")
 
     go = pyqtSignal()
+    worker = None
 
-    def __init__(self):
+    def __init__(self, parent = None):
+        QObject.__init__(self, parent)
         self.ml = pa.pa_mainloop_new()
-        self.worker = Worker(ml)
         self.thread = None
 
     def __del__(self):
-        self.stop()
-        del self.thread
-        del self.worker
         pa.pa_mainloop_free(self.ml)
 
     def get_api(self):
         return pa.pa_mainloop_get_api(self.ml)
 
     def stop(self):
-        self.worker.stop()
+        if self.worker is not None:
+            self.worker.stop()
         self.thread.quit()
         self.thread.wait()
+        del self.thread
+        del self.worker
 
     def start(self):
         self.thread = QThread()
-        self.worker.moveToThread(self.thread)
+        self.worker = self.Worker(self.ml)
         self.go.connect(self.worker.start)
+        self.worker.moveToThread(self.thread)
+        self.thread.start()
+        print("Starting")
         self.go.emit()
 
-class Pulse(QOject):
+class Pulse(QObject):
+
     sink_manager = Manager()
     input_manager = Manager()
 
     _pa_ctx = None
-
-    _c_ctx_sub_success = pa.pa_context_success_cb_t( self._ctx_sub_success )
-    _c_ctx_sub_cb      = pa.pa_context_subscribe_cb_t( self._ctx_sub_cb )
-    _c_ctx_state       = pa.pa_context_notify_cb_t( self._ctx_state )
-    _c_ctx_event       = pa.pa_context_event_cb_t( self._ctx_event )
-    _c_sink_info       = pa.pa_sink_info_cb_t( self._sink_info )
-    _c_input_info      = pa.pa_sink_input_info_cb_t( self._input_info )
 
     @staticmethod
     def _upsert_manage(t, manager, i):
@@ -124,10 +154,10 @@ class Pulse(QOject):
         manager.upsert_item(t(info))
 
     def _sink_info(self, ctx, sink_info, eol, ud):
-        self._upsert_manage(Sink, sink_manager, sink_info)
+        self._upsert_manage(Sink, self.sink_manager, sink_info)
 
     def _input_info(self, ctx, input_info, eol, ud):
-        self._upsert_manage(Sink, input_manager, input_info)
+        self._upsert_manage(Sink, self.input_manager, input_info)
 
     def _ctx_sub_success(self, ctx, success, ud): pass
 
@@ -156,7 +186,7 @@ class Pulse(QOject):
             pa.pa_context_subscribe(ctx,
                                     pa.PA_SUBSCRIPTION_MASK_SINK |
                                     pa.PA_SUBSCRIPTION_MASK_SINK_INPUT,
-                                    _c_ctx_sub_success, None)
+                                    self._c_ctx_sub_success, None)
             print ("Ready!")
 
     def _request_sinks(self, ctx, event=None):
@@ -172,6 +202,14 @@ class Pulse(QOject):
         pa.pa_context_get_sink_input_info(ctx, id, self._c_input_info, event)
 
     def __init__(self, ml):
+        QObject.__init__(self, ml)
+        self._c_ctx_sub_success = pa.pa_context_success_cb_t( self._ctx_sub_success )
+        self._c_ctx_sub_cb      = pa.pa_context_subscribe_cb_t( self._ctx_sub_cb )
+        self._c_ctx_state       = pa.pa_context_notify_cb_t( self._ctx_state )
+        self._c_ctx_event       = pa.pa_context_event_cb_t( self._ctx_event )
+        self._c_sink_info       = pa.pa_sink_info_cb_t( self._sink_info )
+        self._c_input_info      = pa.pa_sink_input_info_cb_t( self._input_info )
+
         self._pa_ctx = pa.pa_context_new(ml.get_api(), c.c_char_p(b'Py_tray_mixer'))
 
         rt = pa.pa_context_connect(self._pa_ctx, None, pa.PA_CONTEXT_NOFLAGS, None)
